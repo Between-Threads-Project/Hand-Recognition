e0129a3e660091fde0e95be59ac9d48bf17dc675
Jolannn <y.narkice@gmail.com>
2026-03-25 20:45:03 +0100

    fix: IP adress

--- scripts/remote.py
+++ scripts/remote.py
@@ -4,6 +4,6 @@ start_hand_tracking(
     [
         ("full", "127.0.0.1", 5055),  # Unity
         ("full", "127.0.0.1", 5057),  # Other python
-        ("small", "RASPBERRY_IP", 5001),  # Raspberry
+        ("small", "100.127.151.6", 5001),  # Raspberry
     ]
 )
